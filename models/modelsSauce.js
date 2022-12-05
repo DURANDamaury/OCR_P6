b692064b3278a7ae4b86a3a1127f82c460b11f5b
DURANDamaury <bdciron@gmail.com>
2022-12-05 15:27:50 +0100

    bug like corrigé, il s'agissait d'une faute de frappe dans le model: defaut=>defauLt

--- models/modelsSauce.js
+++ models/modelsSauce.js
@@ -9,8 +9,8 @@ const sauceSchema = mongoose.Schema(
     mainPepper:     { type: String, required: true },
     imageUrl:       { type: String, required: true },
     heat:           { type: Number, required: true },
-    likes:          { type: Number, defaut: 0 },
-    dislikes:       { type: Number, defaut: 0 },
+    likes:          { type: Number, default: 0},
+    dislikes:       { type: Number, default: 0},
     usersLiked:     { type: [String], default: [] },
     usersDisliked:  { type: [String], default: [] },
     });
